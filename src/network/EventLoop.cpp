108a085fa98b449deb861537ecebd4f941e37788
Tomasz Szymański <tomasz_szymanski@outlook.com>
2026-05-18 13:36:25 +0000

    RequestParser finished Fixes Request Line Fixes #18

--- src/network/EventLoop.cpp
+++ src/network/EventLoop.cpp
@@ -8,14 +8,7 @@ EventLoop::EventLoop()
 
 void EventLoop::run()
 {
-    RequestParser parser;
-    bool ok;
-
     std::cout << "Event loop started" << std::endl;
-    
-    // TEST parseRequestLine
-    ok = parser.parseRequestLine("GET /index.html HTTP/1.1");
-    std::cout << "parseRequestLine result: " << ok << std::endl;
 
     while(1)
     {
