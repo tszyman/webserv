2defb3b6e17751f967e1b1f540268ab4a4b86589
Tomasz Szymanski <tszymans@student.42.fr>
2026-05-23 19:20:08 +0200

    restoring Eventloop (cleaning the tests

--- src/network/EventLoop.cpp
+++ src/network/EventLoop.cpp
@@ -10,22 +10,7 @@ void EventLoop::run()
 {
 	std::cout << "Event loop started" << std::endl;
 
-	// TESTY
-	RequestParser parser;
-	bool ok;
-
-	ok = parser.parseRequestLine("GET /index.html HTTP/1.1");
-	std::cout << "Parsed: " << ok << std::endl;
-	ok = parser.parseHeaderLine("Host: localhost");
-	std::cout << "Parsed: " << ok << std::endl;
-	ok = parser.parseHeaderLine("Content-Length: 42");
-	std::cout << "Parsed: " << ok << std::endl;
-	ok = parser.parseHeaderLine("");
-	std::cout << "Parsed: " << ok << std::endl;
-
-	std::cout << "Headers parsed: " << parser.getHeaders().size() << std::endl;
-
-	while (1)
+	while(1)
 	{
 		// placeholder
 	}
