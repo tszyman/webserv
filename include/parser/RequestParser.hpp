fa462dfa5096a6f16881aaba2a89bcd774ad3f6d
Tomasz Szymanski <tomasz_szymanski@outlook.com>
2026-05-28 23:34:17 +0200

    Body Handling Fixes #20

--- include/parser/RequestParser.hpp
+++ include/parser/RequestParser.hpp
@@ -2,28 +2,67 @@
 #define REQUEST_PARSER_HPP
 #include <string>
 #include <map>
+#include <vector>
+#include <cstdlib>
+
 
 class RequestParser
 {
 	public:
+		enum ParserState{
+			STATE_REQUEST_LINE,
+			STATE_HEADERS,
+			STATE_BODY,
+			STATE_COMPLETE,
+			STATE_ERROR
+		};
+		
 		RequestParser();
-
-		bool parseRequestLine(const std::string &line);
-		bool parseHeaderLine(const std::string &line);
-
+		
+		void feed(const char* data, size_t length);				//feed raw data from the socket into the parser
+		
+		ParserState getState() const;
 		const std::string &getMethod() const;
 		const std::string &getPath() const;
 		const std::string &getVersion() const;
 		const std::map<std::string, std::string> &getHeaders() const;
-
+		const std::vector<char> &getBody() const;
+		
 	private:
+		enum BodyType{
+			BODY_NONE,
+			BODY_CONTENT_LENGTH,
+			BODY_CHUNKED
+		};
+
+		ParserState _state;
+		BodyType _bodyType;
+
 		std::string _method;
 		std::string _path;
 		std::string _version;
-
 		std::map<std::string, std::string> _headers;
+		std::vector<char> _body;
+
+		std::string _headerBuffer;
+		size_t _contentLength;
+		size_t _bytesRead;
+		
+		enum ChunkState{
+			CHUNK_SIZE,
+			CHUNK_DATA,
+			CHUNK_CRLF
+		};
+		ChunkState _chunkState;
+		size_t _currentChunkSize;
+		std::string _chunkHexBuffer;
+
+		void determineBodyType();
+		void processBody(const char* data, size_t length);
 
 		std::string trim(const std::string &str);
+		bool parseRequestLine(const std::string &line);
+		bool parseHeaderLine(const std::string &line);
 };
 
 #endif
--- src/parser/RequestParser.cpp
+++ src/parser/RequestParser.cpp
@@ -1,19 +1,19 @@
 #include "parser/RequestParser.hpp"
 #include <string>
 
-RequestParser::RequestParser()
+RequestParser::RequestParser() 
+	: _state(STATE_REQUEST_LINE), _bodyType(BODY_NONE), 
+	_contentLength(0), _bytesRead(0),
+	_chunkState(CHUNK_SIZE), _currentChunkSize(0)
 {
 }
 std::string RequestParser::trim(const std::string &str)
 {
-	std::string::size_type	start;
-	std::string::size_type	end;
-
-	start = 0;
+	std::string::size_type start = 0;
 	while(start < str.size() && (str[start] == ' ' || str[start] == '\t'))
 		start++;
 
-	end = str.size();
+	std::string::size_type end = str.size();
 	while(end > start && (str[end-1] == ' ' || str[end - 1] == '\t'))
 		end--;
 
@@ -22,14 +22,11 @@ std::string RequestParser::trim(const std::string &str)
 
 bool RequestParser::parseRequestLine(const std::string &line)
 {
-	std::string::size_type pos1;
-	std::string::size_type pos2;
-
-	pos1 = line.find(' ');
+	std::string::size_type pos1 = line.find(' ');
 	if(pos1 == std::string::npos)
 		return false;
 
-	pos2 = line.find(' ', pos1 + 1);
+	std::string::size_type pos2 = line.find(' ', pos1 + 1);
 	if(pos2 == std::string::npos)
 		return false;
 
@@ -51,28 +48,24 @@ bool RequestParser::parseRequestLine(const std::string &line)
 
 bool RequestParser::parseHeaderLine(const std::string &line)
 {
-	std::string::size_type pos;
-	std::string key;
-	std::string value;
-	std::string::size_type i;
-
-	if(line.empty())
+	if(line.empty()){
 		return true;
+	}
 
-	pos = line.find(':');
+	std::string::size_type pos = line.find(':');
 	if(pos == std::string::npos)
 		return false;
 
-	if(line.find(':', pos + 1) != std::string::npos)
-		return false;
+	// if(line.find(':', pos + 1) != std::string::npos)
+	// 	return false;
 
-	key = trim(line.substr(0, pos));
-	value = trim(line.substr(pos + 1));
+	std::string key = trim(line.substr(0, pos));
+	std::string value = trim(line.substr(pos + 1));
 
 	if(key.empty())
 		return false;
 
-	i = 0;
+	size_t i = 0;
 	while(i < key.size())
 	{
 		if(key[i] == ' ' || key[i] == '\t')
@@ -102,4 +95,145 @@ const std::string &RequestParser::getVersion() const
 const std::map<std::string, std::string> &RequestParser::getHeaders() const
 {
 	return _headers;
+}
+
+const std::vector<char> &RequestParser::getBody() const
+{
+	return _body;
+}
+
+RequestParser::ParserState RequestParser::getState() const
+{
+	return _state;
+}
+
+void RequestParser::determineBodyType()
+{
+	std::map<std::string, std::string>::const_iterator it = _headers.find("Transfer-Encoding");
+	if(it != _headers.end() && it->second == "chunked" != std::string::npos)
+	{
+		_bodyType = BODY_CHUNKED;
+		return;
+	}
+	//If not chunked, check for Content-Length
+	it = _headers.find("Content-Length");
+	if(it != _headers.end())
+	{
+		_bodyType = BODY_CONTENT_LENGTH;
+		_contentLength = static_cast<size_t>(std::strtol(it->second.c_str(),NULL,10));
+		_body.reserve(_contentLength);
+		if(_contentLength == 0)
+		{
+			_state = STATE_COMPLETE;
+		}
+		return;
+	}
+
+	//If neither is present, there is no body
+	_bodyType = BODY_NONE;
+	_state = STATE_COMPLETE;
+}
+
+void RequestParser::feed(const char* data, size_t length)
+{
+	if (_state == STATE_ERROR || _state == STATE_COMPLETE)
+		return;
+
+	size_t i = 0;
+	while(i < length && _state != STATE_BODY)
+	{
+		_headerBuffer += data[i];
+		if(_headerBuffer.size() >= 2 && _headerBuffer.substr(_headerBuffer.size() - 2) == "\r\n")
+		{
+			std::string line = _headerBuffer.substr(0, _headerBuffer.size() - 2);
+			_headerBuffer.clear();
+
+			if(line.empty())
+			{
+				_state = STATE_BODY;
+				determineBodyType();
+			} else if(_state == STATE_REQUEST_LINE)
+			{
+				if(!parseRequestLine(line))
+					_state = STATE_ERROR;
+				else
+					_state = STATE_HEADERS;
+			} else if(_state == STATE_HEADERS)
+			{
+				if(!parseHeaderLine(line))
+					_state = STATE_ERROR;
+			}
+		}
+		i++;
+	}
+	// Process the reminder of the buffer as body data
+	if(_state == STATE_BODY && i < length)
+	{
+		processBody(data + i, length - i);
+	}
+}
+
+void RequestParser::processBody(const char* data, size_t length)
+{
+	if(_bodyType == BODY_CONTENT_LENGTH)
+	{
+		size_t bytesToRead = std::min(length, _contentLength - _bytesRead);
+		_body.insert(_body.end(), data, data + bytesToRead);
+		_bytesRead =+ bytesToRead;
+		if(_bytesRead >= _contentLength)
+		{
+			_state = STATE_COMPLETE;
+		}
+	} 
+	else if(_bodyType == BODY_CHUNKED)
+	{
+		size_t i = 0;
+		while(i < length && _state != STATE_ERROR && _state != STATE_COMPLETE)
+		{
+			if(_chunkState == CHUNK_SIZE)
+			{
+				_chunkHexBuffer += data[i++];
+				if(_chunkHexBuffer.size() >= 2 && _chunkHexBuffer.substr(_chunkHexBuffer.size() - 2) == "\r\n")
+				{
+					_currentChunkSize = static_cast<size_t>(std::strtol(_chunkHexBuffer.c_str(), NULL, 16));
+					_chunkHexBuffer.clear();
+					if(_currentChunkSize == 0)
+						_state = STATE_COMPLETE;
+					else
+						_chunkState = CHUNK_DATA;
+				}
+			}
+			else if(_chunkState == CHUNK_DATA)
+			{
+				size_t chunkBytesLeft = _currentChunkSize - _bytesRead;
+				size_t bytesAvailable = length - 1;
+				size_t bytesToCopy = std::min(chunkBytesLeft, bytesAvailable);
+
+				_body.insert(_body.end(), data + i, data + i + bytesToCopy);
+				i += bytesToCopy;
+				_bytesRead += bytesToCopy;
+				if(_bytesRead >= _currentChunkSize)
+				{
+					_chunkState = CHUNK_CRLF;
+					_bytesRead = 0;
+				} 
+			}
+			else if(_chunkState == CHUNK_CRLF)
+			{
+				_chunkHexBuffer += data[i++];
+				if(_chunkHexBuffer.size() == 2)
+				{
+					if(_chunkHexBuffer == "\r\n")
+					{
+						_chunkState = CHUNK_SIZE;
+						_chunkHexBuffer.clear();
+					}
+					else
+					{
+						_state = STATE_ERROR;
+					}
+				}
+			}
+		}
+	}
 }
