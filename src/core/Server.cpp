f32b742c97c677c6ec1e880f00d38557193b5767
bartek3058 <bartek3058@gmail.com>
2026-06-08 10:29:58 +0200

    Address Logic Fixes #13

--- src/core/Server.cpp
+++ src/core/Server.cpp
@@ -6,6 +6,16 @@ Server::Server()
 {   
 }
 
+Server::~Server()
+{
+    std::map<int, Connection*>::iterator it;
+    for (it = _client_connections.begin(); it != _client_connections.end(); ++it)
+    {
+        delete it->second;
+    }
+    _client_connections.clear();
+}
+
 bool Server::loadConfig(int argc, char **argv)
 {
     std::cout << "Loading config..." << std::endl;
@@ -20,4 +30,26 @@ void Server::run()
     std::cout << "Server started" << std::endl;
     EventLoop loop;
     loop.run();
+}
+
+void Server::addConnection(Connection* conn)
+{
+    if (conn != NULL)
+    {
+        int fd = conn->getFd();
+        _client_connections[fd] = conn;
+        std::cout << "[Server] Mapping created for FD: " << fd << std::endl;
+    }
+}
+
+void Server::cleanupConnection(int fd)
+{
+    std::map<int, Connection*>::iterator it = _client_connections.find(fd);
+
+    if (it != _client_connections.end())
+    {
+        std::cout << "[Server] Cleaning up and removing connection for FD: " << fd << std::endl;
+        delete it->second;
+        _client_connections.erase(it);
+    }
 }
