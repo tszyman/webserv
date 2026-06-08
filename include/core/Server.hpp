5ccb7ca8ed2fda597cd2a0b2b9b9a86adfc4da5d
bartek3058 <bartek3058@gmail.com>
2026-06-08 10:29:58 +0200

    Address Logic Fixes #13

--- include/core/Server.hpp
+++ include/core/Server.hpp
@@ -1,12 +1,24 @@
 #ifndef SERVER_HPP
 #define SERVER_HPP
 
+#include <map>
+#include "network/Connection.hpp"
+
 class Server
 {
+    private:
+        std::map<int, Connection*> _client_connections;
+
+        Server(const Server& other);
+        Server& operator=(const Server& other);
+
     public:
         Server();
+        ~Server();
         bool loadConfig(int argc, char **argv);
         void run();
+        void addConnection(Connection* conn);
+        void cleanupConnection(int fd);
 };
 
 #endif
