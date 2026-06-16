0952fdd7f4e69488174adbd855dfe85ee4e734fc
Tomasz Szymanski <tomasz_szymanski@outlook.com>
2026-06-17 00:18:31 +0200

    minor changes to header and methods implemented

--- src/utils/Logger.cpp
+++ src/utils/Logger.cpp
@@ -10,7 +10,7 @@
 #define GRAY	"\033[90m"
 
 // Initialize static variable (default INFO level)
-Logger::Level Logger::_currentLevel = Logger::DEBUG;
+Logger::Level Logger::_currentLevel = Logger::INFO;
 
 void Logger::setLogLevel(Level level){
 	_currentLevel = level;
