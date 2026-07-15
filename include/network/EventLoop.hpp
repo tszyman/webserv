ce0d755e9dd3424c57faa8323259044348c38855
bartek3058 <bartek3058@gmail.com>
2026-07-15 22:03:33 +0200

    load_config + server_run

--- include/network/EventLoop.hpp
+++ include/network/EventLoop.hpp
@@ -7,21 +7,22 @@
 #include <map>
 #include <stdexcept>
 #include <sys/socket.h>
+#include <vector>
 
 class EventLoop
 {
     private:
         Poller          _poller;
-        SocketEngine*   _server_engine;
         bool            _is_running;
 
         std::map<int, Connection*> _connections;
+        std::vector<SocketEngine*> _server_engines;
 
         EventLoop(const EventLoop& other);
         EventLoop& operator=(const EventLoop& other);
 
     public:
-        EventLoop(SocketEngine* engine);
+        EventLoop(const std::vector<SocketEngine*>& engines);
         ~EventLoop();
         void run();
 };
--- src/network/EventLoop.cpp
+++ src/network/EventLoop.cpp
@@ -1,12 +1,16 @@
 #include "network/EventLoop.hpp"
 #include "utils/Logger.hpp"
 #include "utils/StringUtils.hpp"
+#include "http/HttpErrorPage.hpp"
+#include "http/HttpResponse.hpp"
 
-EventLoop::EventLoop(SocketEngine* engine) : _server_engine(engine), _is_running(true)
+EventLoop::EventLoop(const std::vector<SocketEngine*>& engines) : _is_running(true), _server_engines(engines)
 {
-	if(!_server_engine)
-		throw std::runtime_error("EventLoop initialized with NULL SocketEngine");
-	_poller.addFd(_server_engine->getFd(), POLLIN);
+	if(_server_engines.empty())
+		throw std::runtime_error("EventLoop initialized with empty SocketEngine list");
+
+	for (size_t i = 0; i < _server_engines.size(); ++i)
+		_poller.addFd(_server_engines[i]->getFd(), POLLIN);
 }
 
 EventLoop::~EventLoop()
@@ -38,13 +42,26 @@ void EventLoop::run()
 		int current_fd = fds[i].fd;
 		short revents = fds[i].revents;
 
+		bool is_server_socket = false;
+		SocketEngine* active_engine = NULL;
+
+		for (size_t j = 0; j < _server_engines.size(); ++j)
+		{
+			if (current_fd == _server_engines[j]->getFd())
+			{
+				is_server_socket = true;
+				active_engine = _server_engines[j];
+				break;
+			}
+		}
+
 		// 1. IO Multiplexing Safeguard: Detect errors and broken connections
 		if (revents & (POLLERR | POLLHUP | POLLNVAL))
 		{
 			Logger::warning(std::string("Socket error/hangup detected on FD: ") +StringUtils::to_string(current_fd));
 		
 			// If new client socket, clean up memory
-			if (current_fd != _server_engine->getFd())
+			if (!is_server_socket)
 			{
 				delete _connections[current_fd];
 				_connections.erase(current_fd);
@@ -56,15 +73,14 @@ void EventLoop::run()
 		// 2. Main read handling with POLLIN
 		if (revents & POLLIN)
 		{
-			// A: Event on the listening socket (New Client)
-			if (current_fd == _server_engine->getFd())
+			if (is_server_socket)
 			{
-				Connection* new_conn = _server_engine->acceptConnection();
-				if (new_conn)
+				Connection* new_conn = active_engine->acceptConnection();
+				if (new_conn != NULL)
 				{
 					int client_fd = new_conn->getFd();
-					_connections[client_fd] = new_conn;
 					_poller.addFd(client_fd, POLLIN);
+					_connections[client_fd] = new_conn;
 
 					Logger::info(std::string("Accepted and monitoring new client FD: ") + StringUtils::to_string(client_fd));
 				}
@@ -94,42 +110,76 @@ void EventLoop::run()
 			// Successful data read
 			else
 			{
-				buffer[bytes_read] = '\0';
+				Connection* conn = _connections[current_fd];
+				conn->getParser().feed(buffer, bytes_read);
+				RequestParser::ParserState state = conn->getParser().getState();
 
-				Logger::debug(std::string("Received ") + StringUtils::to_string(bytes_read) + " bytes from FD " + StringUtils::to_string(current_fd));
-				std::string mock_http_response = "HTTP/1.1 200 OK\r\nContent-Length: 13\r\n\r\nHello Webserv";
-				_connections[current_fd]->appendResponse(mock_http_response);
+				if (state == RequestParser::STATE_COMPLETE)
+				{
+					Logger::info("Request fully parsed! Method: " + conn->getParser().getMethod() + " Path: " + conn->getParser().getPath());
 
-				_poller.setEvents(current_fd, POLLIN | POLLOUT);
+					HttpResponse response;
+					std::string method = conn->getParser().getMethod();
+					std::string path = conn->getParser().getPath();
+
+					if (method == "GET")
+					{
+						if (path == "/") path = "index.html";
+
+						std::string local_path = "www" + path;
+
+						response.serveStaticFile(local_path);
+					}
+					else
+					{
+						ErrorPage::tryBuildDefault(405, response);
+					}
+
+					conn->appendResponse(response.toString());
+					_poller.setEvents(current_fd, POLLIN | POLLOUT);
+				}
+				else if (state == RequestParser::STATE_ERROR)
+				{
+					Logger::warning("Request parsing error on FD: " + StringUtils::to_string(current_fd));
+
+					HttpResponse response;
+					ErrorPage::tryBuildDefault(400, response);
+
+					conn->appendResponse(response.toString());
+					_poller.setEvents(current_fd, POLLIN | POLLOUT);
+				}
 			}
 		}
 	}
 
 	if (revents & POLLOUT)
 	{
-		Connection* conn = _connections[current_fd];
-		std::string& response = conn->getResponseBuffer();
-
-		if (!response.empty())
+		if(_connections.find(current_fd) != _connections.end())
 		{
-			ssize_t bytes_sent = send(current_fd, response.c_str(), response.size(), 0);
+			Connection* conn = _connections[current_fd];
+			std::string& response = conn->getResponseBuffer();
 
-			if (bytes_sent > 0)
+			if (!response.empty())
 			{
-				Logger::debug(std::string("Sent ") + StringUtils::to_string(bytes_sent) + " bytes to FD " + StringUtils::to_string(current_fd));
-				conn->eraseSentData(bytes_sent);
-			}
-			else if (bytes_sent < 0)
-			{
-				Logger::error(std::string("Send() error on FD: ") + StringUtils::to_string(current_fd));
-			}
+				ssize_t bytes_sent = send(current_fd, response.c_str(), response.size(), 0);
 
-			if(conn->getResponseBuffer().empty())
-			{
-				_poller.setEvents(current_fd, POLLIN);
+				if (bytes_sent > 0)
+				{
+					Logger::debug(std::string("Sent ") + StringUtils::to_string(bytes_sent) + " bytes to FD " + StringUtils::to_string(current_fd));
+					conn->eraseSentData(bytes_sent);
+				}
+				else if (bytes_sent < 0)
+				{
+					Logger::error(std::string("Send() error on FD: ") + StringUtils::to_string(current_fd));
+				}
+
+				if(conn->getResponseBuffer().empty())
+				{
+					_poller.setEvents(current_fd, POLLIN);
+				}
 			}
 		}
 	}
-}
+	}
 	}
 	}
